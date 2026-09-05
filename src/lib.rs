33381227281befdcc3bcc8b9321c7451478a805a
li <li@goldragon.criome.net>
2026-09-05 23:12:51 +0200

    Reserve privileged Lojix wire identity

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,12 @@ pub use generated::*;
 
 pub const META_LOJIX_SIGNAL_SOURCE: &str = include_str!("../ethos/lib.ethos");
 
-/// The allocated privileged Lojix wire contract: seat 2, structural revision 3.
+/// The allocated privileged Lojix wire contract: stable seat 6, structural revision 3.
 pub enum MetaLojixWire {}
 
 impl signal_frame::WireContract for MetaLojixWire {
     const BINDING: signal_frame::ContractBinding = signal_frame::ContractBinding::new(
-        signal_frame::ContractId::new(core::num::NonZeroU32::new(2).expect("meta seat is nonzero")),
+        signal_frame::ContractId::new(core::num::NonZeroU32::new(6).expect("meta Lojix seat is nonzero")),
         signal_frame::WireRevision::new(core::num::NonZeroU16::new(3).expect("meta revision is nonzero")),
     );
 }
